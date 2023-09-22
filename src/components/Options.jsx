a79497a206f42794059b07bef13bcbc3c9391abf
Aakash-Developer <aakash20899macwan@gmail.com>
2023-09-22 06:49:49 +0530

    up

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -2,17 +2,14 @@ import React from "react";
 
 export default function Options({ qusetions, dispatch, answer }) {
   const hasAnswer = answer !== null;
-
+  console.log(qusetions);
   return (
     <div className="flex flex-col gap-3 ">
       {qusetions.options.map((option, index) => (
         <button
           key={option}
-          className={`p-2 rounded-full bg-slate-900 hover:bg-slate-800 active:bg-slate-900 hover:relative hover:scale-105 transition-all ease-in duration-200 ${
-            index === answer ? "bg-teal-600" : " "
-          } ${hasAnswer ? (index === qusetions.correctAnswer ? "bg-green-700" : "bg-red-600 cursor-not-allowed") : ""}  `}
-          onClick={() => dispatch({ type: "newAnswer", payload: index })}
-          disabled={hasAnswer}>
+          className={`answerBtn ${index === answer ? "bg-teal-700 relative -left-4" : ""} ${hasAnswer ? (index === qusetions.correctAnswer ? "bg-green-700" : "bg-red-700") : ""}`}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}>
           {option}
         </button>
       ))}
